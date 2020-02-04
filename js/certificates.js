dcf3b9ea19e54fad1ced754a1eb1ae111d9359eb
HimanshuRattan <35609063+HimanshuRattan@users.noreply.github.com>
2020-02-04 23:44:08 +0530

    new event and recruimtment

--- js/certificates.js
+++ js/certificates.js
@@ -132,3 +132,41 @@ function dod2() {
 			console.log('OOPS!');
 		});
 }
+//BIG DATA ANALYTICS AND PRODUCT MODELING
+function dod3() {
+	document.getElementById('progress').style.visibility = 'visible';
+	var storageRef = storage.ref();
+	var regno = document.getElementById('regid').value;
+	if (regno.length < 5) {
+		console.log(regno.length);
+		document.getElementById('datalink').innerHTML = '';
+		document.getElementById('datalink').style.visibility = 'hidden';
+		document.getElementById('errorlink').innerHTML = 'Please enter your email correctly';
+		document.getElementById('errorlink').style.visibility = 'visible';
+		document.getElementById('progress').style.visibility = 'hidden';
+		return;
+	}
+	var filepath = 'BigData//' + regno + '.pdf';
+	var pathReference = storage.ref(filepath);
+
+	storageRef
+		.child(filepath)
+		.getDownloadURL()
+		.then(function(url) {
+			document.getElementById('datalink').innerHTML = 'Download Certificate';
+			document.getElementById('datalink').href = url;
+			document.getElementById('datalink').style.visibility = 'visible';
+			document.getElementById('errorlink').style.visibility = 'hidden';
+			document.getElementById('progress').style.visibility = 'hidden';
+			console.log('FOUND IT BABY!');
+		})
+		.catch(function(error) {
+			document.getElementById('datalink').innerHTML = '';
+			document.getElementById('datalink').style.visibility = 'hidden';
+			document.getElementById('errorlink').innerHTML =
+				"Hey! We couldn't find any certificate with this email. Make sure you have entered it correctly. Contact us if you think it's our fault.";
+			document.getElementById('errorlink').style.visibility = 'visible';
+			document.getElementById('progress').style.visibility = 'hidden';
+			console.log('OOPS!');
+		});
+}
